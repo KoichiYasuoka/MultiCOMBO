81bc6af5f0ff8cf952168fe53f2fa0258b3c0ebb
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2022-02-02 14:34:57 +0900

    version 0.8.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/MultiCOMBO"
 
 setuptools.setup(
   name="multicombo",
-  version="0.8.2",
+  version="0.8.3",
   description="Multilingual POS-tagger and Dependency-parser",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setuptools.setup(
   license="GPL",
   keywords="NLP Multilingual",
   packages=setuptools.find_packages(),
-  install_requires=["unidic_combo>=1.3.4","transformers>=4.5.1"],
+  install_requires=["unidic_combo>=1.3.6","transformers>=4.5.1"],
   python_requires=">=3.6",
   package_data={"multicombo":["download/*.txt"]},
   classifiers=[
